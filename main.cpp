a90881d847f23d9bd405dae1eb6d9f984f6ff01d
hutchinsonmouray <80347718+hutchinsonmouray@users.noreply.github.com>
2021-11-30 15:56:41 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
-#include 
+#include <iostream>
+#include "Graph.cpp"
+
+void pageCount(int min, int max, vector<Node> &books) {
+    vector<Node> newBooks;
+    //deletes all old books and adds new books w/ correct page to vector
+    for (int i=0; i<books.size();i++) {
+        if (books.back().pageCount>=min && books.back().pageCount<=max)
+            newBooks.push_back(books.back());
+
+            books.pop_back();
+    }
+
+    books = newBooks; //now the new books will be filtered
+}
+void  ratingSort(vector<Node> &books) {
+    //linear sort?
+    //utilize ordered map
+    //attempt a quicksort alg using a given pseudo code?
+    //trying to decide which is faster
+    
+}
+
+
+
+int main() {
+    return 0;
+}
