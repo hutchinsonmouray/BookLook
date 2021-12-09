05316f406cd1a9caeb698486275cbe4e6cc46033
Morgan Cobb <73551445+morgan-cobb@users.noreply.github.com>
2021-12-09 01:19:29 -0500

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -196,7 +196,7 @@ targGenre2 = genre2;
 
 void Graph::readFile(){
 
-    ifstream file("GoodReads_100k_books.csv");
+    ifstream file("GoodReads_Tilde.csv");
     string input;
     getline(file, input);
     int i=0;
@@ -207,43 +207,43 @@ void Graph::readFile(){
         Node* temp = new Node();
         string token;
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->author = token;
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->bookFormat = token;
-	    
-	getline(myData, token, ',');
+
+        getline(myData, token, '~');
         temp->desc = token;
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->genre = token;
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->img = stoi(token);
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->isbn= token;
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->isbn13 = token;
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->link= token;
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->pageCount = stoi(token);
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->rating = stod(token);
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->review= token;
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->title = token;
 
-        getline(myData, token, ',');
+        getline(myData, token, '~');
         temp->totalRatings= token;
 
 
